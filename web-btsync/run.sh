bfeb84fbd6b778a6fa077358e705df9c22e32558
Luigi Maselli <grigio.org@gmail.com>
2015-04-28 11:22:16 +0200

    upd web-btsync

--- web-btsync/run.sh
+++ web-btsync/run.sh
@@ -1,1 +1,3 @@
+docker run -it -v $PWD/syncdata:/data -p 55555:55555 -p 9106:8888 --name="web-btsync" grigio/web-btsync /bin/bash
+
 docker run -d --restart=always -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged --volumes-from=sync -p 55555:55555 -p 9106:8888 --name="web-btsync" grigio/web-btsync
